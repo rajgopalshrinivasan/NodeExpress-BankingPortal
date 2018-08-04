82097db29e5fdfcfb0a16069726191c228c3bdb7
srajgopal <srajgopal@hotmail.com>
2018-08-04 15:42:43 -0400

    writeJSON

--- src/app.js
+++ src/app.js
@@ -50,7 +50,9 @@ app.post('/transfer',(req, res) => {
     accounts[req.body.from].balance = accounts[req.body.from].balance  - req.body.amount;
     accounts[req.body.to].balance = accounts[req.body.to].balance  + parseInt(req.body.amount, 10);
     const accountsJSON  = JSON.stringify(accounts)
-    writeJSON('json/accounts.json',accountsJSON)
+    //writeJSON('json/accounts.json',accountsJSON)
+    writeJSON()
+
     res.render("transfer", {message: "Transfer Completed"})
 })
 
@@ -62,7 +64,8 @@ app.post('/payment',(req, res) => {
     accounts.credit.balance = accounts.credit.balance - req.body.amount;
     accounts.credit.available = accounts.credit.available + parseInt(req.body.amount, 10);
     const accountsJSON  = JSON.stringify(accounts)
-    writeJSON('json/accounts.json',accountsJSON)
+    //writeJSON('json/accounts.json',accountsJSON)
+    writeJSON()
     res.render("payment", {account: accounts.credit, message: "Payment Successful"})
 })
 
--- src/data.js
+++ src/data.js
@@ -9,8 +9,12 @@ const accounts = JSON.parse(accountData)
 const userData  = fs.readFileSync('src/json/users.json','UTF8')
 const users = JSON.parse(userData)
 
-const writeJSON = (fileName, jsonObject) => {
-    fs.writeFileSync(path.join(__dirname, fileName),jsonObject ,'utf8')
+// const writeJSON = (fileName, jsonObject) => {
+//     fs.writeFileSync(path.join(__dirname, fileName),jsonObject ,'utf8')
+//   }
+
+const writeJSON = () => {
+    
   }
 
 module.exports  = {accounts,users,writeJSON }
