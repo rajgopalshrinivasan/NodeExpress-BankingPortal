73dfd31034fd6c472086739e09b7a875d1b1bf02
srajgopal <srajgopal@hotmail.com>
2018-08-04 16:00:03 -0400

    routes

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const path = require('path')
 const {accounts,users, writeJSON } = require('./data.js')
-
+const accountRoutes = require('./routes/accounts.js')
+const servicesRoutes  = require('./routes/services.js')
 const express = require('express')
 
 const app = express()
@@ -18,23 +19,9 @@ app.get('/', (req, res) => {
     })
 })
 
-app.get('/savings', (req, res) => {
-    res.render("account",{
-        account: accounts.savings
-    })
-})
+app.use('/account', accountRoutes )
+app.use('/services', servicesRoutes )
 
-app.get('/checking', (req, res) => {
-    res.render("account",{
-        account: accounts.checking
-    })
-})
-
-app.get('/credit', (req, res) => {
-    res.render("account",{
-        account: accounts.credit
-    })
-})
 
 app.get('/profile', (req, res) => {
     res.render("profile",{
@@ -42,32 +29,7 @@ app.get('/profile', (req, res) => {
     })
 })
 
-app.get('/transfer',(req, res) => {
-    res.render("transfer")
-})
 
-app.post('/transfer',(req, res) => {
-    accounts[req.body.from].balance = accounts[req.body.from].balance  - req.body.amount;
-    accounts[req.body.to].balance = accounts[req.body.to].balance  + parseInt(req.body.amount, 10);
-    //const accountsJSON  = JSON.stringify(accounts)
-    //writeJSON('json/accounts.json',accountsJSON)
-    writeJSON()
-
-    res.render("transfer", {message: "Transfer Completed"})
-})
-
-app.get('/payment',(req, res) => {
-    res.render("payment",{account: accounts.credit})
-})
-
-app.post('/payment',(req, res) => {
-    accounts.credit.balance = accounts.credit.balance - req.body.amount;
-    accounts.credit.available = accounts.credit.available + parseInt(req.body.amount, 10);
-    //const accountsJSON  = JSON.stringify(accounts)
-    //writeJSON('json/accounts.json',accountsJSON)
-    writeJSON()
-    res.render("payment", {account: accounts.credit, message: "Payment Successful"})
-})
 
 app.listen(3000, () => {
 console.log('PS Project Running on port 3000!')
